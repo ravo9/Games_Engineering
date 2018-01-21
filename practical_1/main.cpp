26686af997c3b275feb4720842e4cc5d803c6849
Rafal Ozog <rafalozog@gmail.com>
2018-01-21 15:11:54 +0000

    Pong game done.

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,4 +1,5 @@
 #include  <SFML/Graphics.hpp>
+#include <string> 
 
 using  namespace  sf;
 using  namespace  std;
@@ -7,7 +8,7 @@ const  Keyboard ::Key  controls [4] = {
 	Keyboard ::A,    //  Player1  UP
 	Keyboard ::Z,    //  Player1  Down
 	Keyboard ::Up,   //  Player2  UP
-	Keyboard ::Down //  Player2  Down
+	Keyboard ::Down  //  Player2  Down
 };
 
 const  Vector2f  paddleSize (25.f, 100.f);
@@ -19,8 +20,29 @@ const  float  paddleSpeed = 400.f;
 Vector2f  ballVelocity;
 bool  server = false;
 
+int scoreLeft = 0;
+int scoreRight = 0;
+
+Text text;
+Font font;
+
 CircleShape  ball;
 RectangleShape  paddles [2];
+
+void Reset() {
+	//  reset  paddle  position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(790 - paddleSize.x / 2, gameHeight / 2);
+
+	//  reset  Ball  Position
+	ball.setPosition(400 - ballRadius, gameHeight / 2);
+	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+
+	// Score display
+	text.setString(to_string(scoreLeft) + " : " +  to_string(scoreRight));
+	text.setPosition((gameWidth * 0.5f) - 35, 20);
+	//text.setPosition((gameWidth * 0.5f) - (text.getLocalBounds().width * 0.5f), 0);
+}
 
 void  Load() {
 	// Set  size  and  origin  of  paddles
@@ -33,14 +55,12 @@ void  Load() {
 	ball.setRadius(ballRadius  - 3);
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
 
-	//  reset  paddle  position
-	paddles [0]. setPosition (10 + paddleSize.x / 2, gameHeight / 2);
-	paddles [1]. setPosition (790 - paddleSize.x / 2, gameHeight / 2);
+	Reset();
 
-	//  reset  Ball  Position
-	ball.setPosition (400 - ballRadius, gameHeight / 2);
-
-	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
+	// Font load
+	font.loadFromFile("res/fonts/RobotoMono-Regular.ttf");
+	text.setFont(font);
+	text.setCharacterSize(24);
 }
 
 void  Update(RenderWindow &window) {
@@ -63,15 +83,33 @@ void  Update(RenderWindow &window) {
 		window.close ();
 	}
 
-	//  handle  paddle  movement
-	float  direction = 0.0f;
-	if (Keyboard :: isKeyPressed(controls [0])) {
-		direction --;
+	// Handle  paddle  movement
+	float  directionA = 0.0f;
+	float  directionB = 0.0f;
+	if (Keyboard :: isKeyPressed(controls [0]) && (paddles[0].getPosition().y > 0)) {
+		directionA --;
 	}
-	if (Keyboard :: isKeyPressed(controls [1])) {
-		direction ++;
+	if (Keyboard :: isKeyPressed(controls [1]) && (paddles[0].getPosition().y < gameHeight)) {
+		directionA ++;
 	}
-	paddles [0]. move(0,  direction * paddleSpeed * dt);
+
+	/*
+	if (Keyboard::isKeyPressed(controls[2]) && (paddles[1].getPosition().y > 0)) {
+		directionB--;
+	}
+	if (Keyboard::isKeyPressed(controls[3]) && (paddles[0].getPosition().y < gameHeight)) {
+		directionB++;
+	}
+	*/
+
+	// AI logic
+	if (ball.getPosition().y < paddles[1].getPosition().y)
+		directionB--;
+	else if (ball.getPosition().y > paddles[1].getPosition().y)
+		directionB++;
+		
+	paddles[0].move(0,  directionA * paddleSpeed * dt);
+	paddles[1].move(0, directionB * paddleSpeed * dt);
 
 	ball.move(ballVelocity * dt);
 
@@ -90,6 +128,24 @@ void  Update(RenderWindow &window) {
 		ballVelocity.y *= -1.1f;
 		ball.move(0, 10);
 	}
+	else if (bx > gameWidth) {
+		// right wall
+		scoreLeft++;
+		Reset();
+	}
+	else if (bx < 0) {
+		// left wall
+		scoreRight++;
+		Reset();
+	}
+	else if (bx < paddleSize.x && by > paddles[0].getPosition().y -(paddleSize.y * 0.5) && by < paddles[0].getPosition().y + (paddleSize.y * 0.5)) {
+		// left paddle
+		ballVelocity.x *= -1;
+	}
+	else if (bx > gameWidth - paddleSize.x && by > paddles[1].getPosition().y - (paddleSize.y * 0.5) && by < paddles[1].getPosition().y + (paddleSize.y * 0.5)) {
+		// right paddle
+		ballVelocity.x *= -1;
+	}
 }
 
 void  Render(RenderWindow &window) {
@@ -97,6 +153,7 @@ void  Render(RenderWindow &window) {
 	window.draw(paddles[0]);
 	window.draw(paddles[1]);
 	window.draw(ball);
+	window.draw(text);
 }
 
 int  main() {
